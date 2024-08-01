3a56800ff2903e3a2b4d710f84e20f895df7069d
Sabeer2002 <sabeer2002ahmed@gmail.com>
2024-08-01 15:53:50 +0530

    Implementing multiple inheritence using Interface in java

--- myex28.java
+++ myex28.java
@@ -1,63 +1,74 @@
+
 //multiole inheritance using interface
 import java.util.Scanner;
-interface InOut{
-void input();
-void read();
-void output();
-}
-interface Process{
-void area();
-void vol();
-}
-class Circle implements InOut,Process
-{
-double r,a;
-Scanner sc=new Scanner(System.in);
-public void input(){
-System.out.println("Enter radius");
-r=sc.nextDouble();
+
+interface InOut {
+	void input();
+
+	void read();
+
+	void output();
 }
 
-public void area(){
-a=3.14*r*r;
+interface Process {
+	void area();
+
+	void vol();
 }
 
-public void output(){
-System.out.println("Area of circle= "+a);
-System.out.println("Radius of circle= "+r);
+class Circle implements InOut, Process {
+	double r, a;
+	Scanner sc = new Scanner(System.in);
+
+	public void input() {
+		System.out.println("Enter radius");
+		r = sc.nextDouble();
+	}
+
+	public void area() {
+		a = 3.14 * r * r;
+	}
+
+	public void output() {
+		System.out.println("Area of circle= " + a);
+		System.out.println("Radius of circle= " + r);
+	}
+
+	public void read() {
+	};
+
+	public void vol() {
+	};
 }
 
-public void read(){};
-public void vol(){};
+class Cylinder extends Circle implements InOut, Process {
+	double h, v;
+
+	public void input() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Height:");
+		h = sc.nextDouble();
+		super.input();
+	}
+
+	public void vol() {
+		super.area();
+		v = a * h;
+	}
+
+	public void output() {
+		super.output();
+		System.out.println("Height of Cylinder= " + h);
+		System.out.println("Volume of Cylinder= " + v);
+	}
 }
 
-class Cylinder extends Circle implements InOut,Process
-{
-double h,v;
-
-public void input(){
-
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the Height:");
-h=sc.nextDouble();
-super.input();
-}
-public void vol(){
-super.area();
-v=a*h;
-} 
-public void output(){
-super.output();
-System.out.println("Height of Cylinder= "+h);
-System.out.println("Volume of Cylinder= "+v);
-}
-}
-
-class MulInterfaceMain{
-public static void main(String...arg){
-Cylinder c=new Cylinder();
-c.input();
-c.vol();
-c.output();
-}
+class MulInterfaceMain {
+	public static void main(String... arg) {
+		Cylinder c = new Cylinder();
+		c.input();
+		c.vol();
+		c.output();
+	}
 }
